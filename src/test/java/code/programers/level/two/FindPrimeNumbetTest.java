f2c61a8926963094eb2f0dc31ebe835007345e6c
junho9341 <junho9341@gmail.com>
2021-08-13 15:19:23 +0900

    delete : 불필요 클래스

--- src/test/java/code/programers/level/two/FindPrimeNumbetTest.java
+++ src/test/java/code/programers/level/two/FindPrimeNumbetTest.java
@@ -2,36 +2,74 @@ package code.programers.level.two;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.junit.jupiter.api.Test;
+
+import datatype.Number;
+import solution.Sieve;
 
 public class FindPrimeNumbetTest {
 
     @Test
     void testCaseOne()
     {
-        FindPrimeNumber fpn = new FindPrimeNumber();
+        Sieve sieve = new Sieve();
+        BiggestNumber bn = new BiggestNumber();
+        AlterKey ak = new AlterKey();
+
+        bn.list = new ArrayList<>();
+        int max = 0;
+        Map<Integer, Boolean> map = new HashMap<>();
+        int count = 1;
 
         String numbers = "011";
-        int[] str_array = new int[numbers.length()];
+        int[] strArray = new int[numbers.length()];
 
-        for(int index=0; index< str_array.length; index++)
-            str_array[index] = Integer.parseInt(numbers.substring(index, index+1));
+        for(int index=0; index< strArray.length; index++)
+            strArray[index] = Integer.parseInt(numbers.substring(index, index+1));
 
+        int[] akArray = ak.columns(strArray.length);
+        int[][] combination = ak.dimension(akArray);
 
-        int count = fpn.solution(str_array);
+        for(int[] arr:combination){
+            bn.list.add(arr);
+        }
+
+        while(count < strArray.length){
+            count++;
+
+            combination = ak.combination(akArray, combination);
+
+            for(int[] arr: combination){
+                bn.permutation(arr, 0);
+            }
+        }
+
+        for(int[] arr:bn.list){
+            int number = Number.extractInt(arr, strArray);
+
+            map.put(number, map.getOrDefault(number, false));
+
+            if(max < number){
+                max = number;
+            }
+        }
+
+        sieve.length(max + 1);
+        sieve.visitIndex(max);
+
+        count = 0;
+
+        for(Entry<Integer, Boolean> entry : map.entrySet()){
+            if(sieve.getList().contains(entry.getKey())){
+                count++;
+            }
+        }
 
         assertEquals(2, count);
-    }
-
-    @Test
-    void testCaseTwo()
-    {
-        FindPrimeNumber fpn = new FindPrimeNumber();
-
-        int[] array = new int[]{1,7};
-
-        int count = fpn.solution(array);
-
-        assertEquals(3, count);
     }
 }
